0efc745e68c6e13fd8aea6e0d4441024b308b1b4
Marcelo Lorenzo <mpdlorenzo@gmail.com>
2022-06-24 10:58:00 +0200

    instala firebase

---
+++ front-end/src/services/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxpIek7hLAU4yOdmAW7BMyGT5sjpOxQ9I",
+  authDomain: "new-york-delivery-bdb00.firebaseapp.com",
+  projectId: "new-york-delivery-bdb00",
+  storageBucket: "new-york-delivery-bdb00.appspot.com",
+  messagingSenderId: "858461805259",
+  appId: "1:858461805259:web:cd0d8471aaf3d57bf7194e"
+};
+
+const app = initializeApp(firebaseConfig);
