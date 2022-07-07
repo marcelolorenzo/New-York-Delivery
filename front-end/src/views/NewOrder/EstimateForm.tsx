986d8ed31fd4b39156d75246c89ea99ab6f7b1c3
Marcelo Lorenzo <mpdlorenzo@gmail.com>
2022-07-07 16:03:31 +0200

    envia estimativa para Redux

--- front-end/src/views/NewOrder/EstimateForm.tsx
+++ front-end/src/views/NewOrder/EstimateForm.tsx
@@ -6,6 +6,8 @@ import { FormField } from "../../components/FormField";
 import { Address } from "../../entities/Address";
 import * as yup from 'yup';
 import { createEstimate, NewEstimateInput } from "../../services/createEstimate";
+import { useDispatch } from "react-redux";
+import { setCurrentEstimate } from "../../store/slices/estimateSlice";
 
 type FormValues = {
     pickupAddress: Address | null
@@ -14,6 +16,7 @@ type FormValues = {
 }
 
 export function EstimateForm() {
+    const dispatch = useDispatch()
     const formik = useFormik<FormValues>({
         initialValues: {
             pickupAddress: null,
@@ -29,7 +32,8 @@ export function EstimateForm() {
             .required('Inform the instructions.')
         }),
         onSubmit: async (values) => {
-           await createEstimate(values as NewEstimateInput)
+           const estimate = await createEstimate(values as NewEstimateInput)
+           dispatch(setCurrentEstimate(estimate ))
         }
     })
     const getFieldProps = (fieldName: keyof FormValues) => {
