5944b356ef3e0705582d89fea699d8aa691669cd
Marcelo Lorenzo <mpdlorenzo@gmail.com>
2022-07-10 11:24:10 +0200

    salva pedido no firebase

--- front-end/src/views/NewOrder/EstimateFinish.tsx
+++ front-end/src/views/NewOrder/EstimateFinish.tsx
@@ -3,15 +3,26 @@ import { selectCurrentEstimate } from "../../store/slices/estimateSlice";
 import { OrderResponseBody } from '@paypal/paypal-js'
 import { toast } from "react-toastify";
 import { PaypalButton } from "../../components/PaypalButton";
+import { createOrder } from "../../services/createOrder";
+import { selectUser } from "../../store/slices/userSlice";
 
 
 export function EstimateFinish() {
     const currentEstimate = useSelector(selectCurrentEstimate)
-    if (!currentEstimate) {
+    const user = useSelector(selectUser)
+    if (!currentEstimate || !user) {
         return null
     }
     const handlePaypalSuccess = async (details: OrderResponseBody) => {
-
+        try {
+            await createOrder({
+                estimate: currentEstimate,
+                gatewayId: details.id,
+                userId: user.id
+            })
+        } catch {
+            toast.error('Failed to process the payment. Please try again.')
+        }
     }
     const handlePaypalError = () => {
         toast.error('An error occurred. Please try again.')
